286c06b3fde7b5173105fce0dc8d91b6e597ed89
DeYangLiu <liu_de_yang@qq.com>
2018-08-30 12:32:13 +0800

    setting: emacs25.3/26.1 有中文会卡顿

--- elisp/init.el
+++ elisp/init.el
@@ -196,7 +196,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "宋体" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
 
 ;;;display
 ;;;; cursor display M-x hl-line-mode or global-hl-line-mode
